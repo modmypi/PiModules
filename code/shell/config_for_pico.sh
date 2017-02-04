23e4928015acc91a4507dd3e63b9a490eaf2495b
Crescendo Fang <crescendofang@gmail.com>
2017-02-04 08:53:22 +0700

    Daemon Installation Script
    No need to mix up daemon installation and firmware update preparation step.

--- code/shell/config_for_pico.sh
+++ code/shell/config_for_pico.sh
@@ -18,24 +18,12 @@ pip install psutil
 echo '--- pip install xmltodict'
 pip install xmltodict
 
-echo '--- save and edit cmdline.txt'
-cp /boot/cmdline.txt /boot/cmdline.txt.save
-sed -i 's| console=serial0,115200 console=tty1||' /boot/cmdline.txt
-
-echo '--- save and edit config.txt'
-cp /boot/config.txt /boot/config.txt.save
-sed -i 's|#dtparam=i2c_arm=on|dtparam=i2c_arm=on|' /boot/config.txt
-
-
 echo '--- adding line to config.txt'
-echo -e "\n\ndtparam=pi3-disable-bt\n\n" >> /boot/config.txt
+echo -e "\n\ndtoverlay=i2c-rtc,ds1307\n\n" >> /boot/config.txt
+echo -e "\n\nenable_uart=1\n\n" >> /boot/config.txt
 
 echo '--- adding lines to /etc/modules'
-echo -e "\n\ni2c-bcm2708\ni2c-dev\n\n" >> /etc/modules
-
-echo '--- disabling hciuart'
-systemctl disable hciuart
+echo -e "\n\ni2c-bcm2708\ni2c-dev\rtc-ds1307\n\n" >> /etc/modules
 
 echo '--- all done'
 exit 0
-
