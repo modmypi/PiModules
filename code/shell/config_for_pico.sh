a471588086e0bfc1261038381fd56acd7a62dbda
asr <andre@spahlenbach.de>
2020-11-22 21:15:17 +0100

    Changes for py3

--- code/shell/config_for_pico.sh
+++ code/shell/config_for_pico.sh
@@ -10,13 +10,13 @@ set -e
 echo '--- update'
 apt-get update
 echo '--- install some packages'
-apt-get install -y python3-dev python3-pip python3-serial python3-smbus python3-jinja2 wiringpi
+apt-get install -y python3-dev python3-pip python3-serial python3-smbus python3-jinja2 python3-rpi.gpio wiringpi
 
 echo '--- pip install psutil'
-pip install psutil
+pip3 install psutil
 
 echo '--- pip install xmltodict'
-pip install xmltodict
+pip3 install xmltodict
 
 echo '--- save and edit cmdline.txt'
 cp /boot/cmdline.txt /boot/cmdline.txt.save
@@ -26,30 +26,36 @@ echo '--- save and edit config.txt'
 config="/boot/config.txt"
 cp $config /boot/config.txt.save
 
+echo '--- enable i2c'
 if grep -R "dtparam=i2c_arm=on" $config
 then
-	sed -i 's|#dtparam=i2c_arm=on|dtparam=i2c_arm=on|' $config
+        sed -i 's|#dtparam=i2c_arm=on|dtparam=i2c_arm=on|' $config
 else
-	echo 'dtparam=i2c_arm=on' $config
+        echo 'dtparam=i2c_arm=on' $config
 fi
 
+echo '--- enable uart'
 raspiuart=`cat $config | grep enable_uart`
 if [ "$raspiuart" == "#enable_uart=1" ]; then
-	sed -i "s,$raspiuart,enable_uart=1," $config
+        sed -i "s,$raspiuart,enable_uart=1," $config
 elif [ "$raspiuart" == "#enable_uart=0" ]; then
-	sed -i "s,$raspiuart,enable_uart=1," $config
+        sed -i "s,$raspiuart,enable_uart=1," $config
 elif [ "$raspiuart" == "enable_uart=0" ]; then
-	sed -i "s,$raspiuart,enable_uart=1," $config
+        sed -i "s,$raspiuart,enable_uart=1," $config
 else
-	sh -c "echo 'enable_uart=1' >> $config"
+        sh -c "echo 'enable_uart=1' >> $config"
 fi
 
+echo '--- enable rtc'
 ### Checking if rtc dtoverlay module is loaded which doesn't work on older kernels
-	rtcmodule=`cat $config | grep dtoverlay=i2c-rtc,ds1307`
-	if [ "$rtcmodule" == "#dtoverlay=i2c-rtc,ds1307" ]; then
-		sed -i -e 's/dtoverlay=i2c-rtc,ds1307/dtoverlay=i2c-rtc,ds1307/g' $config
-	fi
-	sleep 1
+#rtcmodule=`cat $config | grep dtoverlay=i2c-rtc,ds1307`
+rtcmodule=""
+if [ "$rtcmodule" == "#dtoverlay=i2c-rtc,ds1307" ]; then
+        sed -i -e 's/#dtoverlay=i2c-rtc,ds1307/dtoverlay=i2c-rtc,ds1307/g' $config
+else
+        sh -c "echo 'dtoverlay=i2c-rtc,ds1307' >> $config"
+fi
+sleep 1
 
 
 echo '--- disabling hciuart'
@@ -60,9 +66,9 @@ apt-get -y remove fake-hwclock
 update-rc.d -f fake-hwclock remove
 systemctl disable fake-hwclock
 
-sed ':a;N;$!ba;s/if \[ -e \/run\/systemd\/system \] ; then\n    exit 0\nfi/#if \[ -e \/run\/systemd\/system \] ; then\n#    exit 0\n#fi/g' /lib/udev/hwclock-set
-sed -i -e 's/\/sbin\/hwclock --rtc=$dev --systz --badyear/#\/sbin\/hwclock --rtc=$dev --systz --badyear/g' /lib/udev/hwclock-set
-sed -i -e 's/\/sbin\/hwclock --rtc=$dev --systz/#\/sbin\/hwclock --rtc=$dev --systz/g' /lib/udev/hwclock-set
+sed -i ':a;N;$!ba;s/if \[ -e \/run\/systemd\/system \] ; then\n    exit 0\nfi/#if \[ -e \/run\/systemd\/system \] ; then\n#    exit 0\n#fi/g' /lib/udev/hwclock-set
+sed -i -e 's/  \/sbin\/hwclock --rtc=$dev --systz --badyear/  #\/sbin\/hwclock --rtc=$dev --systz --badyear/g' /lib/udev/hwclock-set
+sed -i -e 's/  \/sbin\/hwclock --rtc=$dev --systz/  #\/sbin\/hwclock --rtc=$dev --systz/g' /lib/udev/hwclock-set
 #do later in fabfile
 #hwclock -w
 
